cb660c1f9a933031fefce30d761145e3976474d0
franciscodeppe <franciscodeppe@github.com>
2017-07-14 13:36:42 -0700

    omdb working

--- dudebro.js
+++ dudebro.js
@@ -1,5 +1,7 @@
 let fs = require("fs");
 
+var keys = require("./keys.js");
+
 var action = process.argv[2]
 var value = process.argv[3]
 
@@ -19,7 +21,9 @@ switch (action) {
 }
 
 function spotifySearch() {
-  console.log("Spotify Hello!")
+  // console.log("Spotify Hello!")
+
+  var key = keys.spotifykey
   console.log(value + " is a song")
 //   let track = query;
 //   let queryURL = "https://api.spotify.com/v1/search?q=" + track + "&type=track";
@@ -32,21 +36,34 @@ function spotifySearch() {
 }
 
 function omdbSearch() {
-  console.log("Movie Hello!")
-  console.log(value + " is a movie")
+  // console.log("Movie Hello!")
+  // console.log(value + " is a movie")
 
-  // let movie;
-  // let queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json";
-  //
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET"
-  // }).done(function(response) {
-  //
-  //
-  // });
+  let APIkey = Object.values(keys.omdbKey)
+
+  // console.log(APIkey)
+
+  let queryURL = "http://www.omdbapi.com/?apikey=" + APIkey + "&t=" + value + "&y=&plot=short&r=json";
+
+  let request = require("request");
+  request.post(queryURL, {json: true}, function(err, response, body) {
+    if (err) {
+      console.log(err)
+    }
+    console.log(
+      "Title: " + body.Title +
+      "\nYear: " + body.Year +
+      "\nIMDB Rating: " + body.Ratings.imdbRating +
+      "\nRottenTomato Rating: " + body.Ratings[0[2]] +
+      "\nCountry: " + body.Country +
+      "\nLanguage: " + body.Language +
+      "\nPlot: " + body.Plot +
+      "\nActors: " + body.Actors
+      )
+  })
 }
-
 function twitterSearch() {
-  console.log("Twitter Hello!")
+  // console.log("Twitter Hello!")
+
+  var key = APIkey.omdbkeys
 }
--- keys.js
+++ keys.js
@@ -1,16 +1,16 @@
-console.log("this is loaded");
+// console.log("this is loaded");
 
-exports.twitterKeys = {
+exports.twitterKey = {
 	consumer_key: "mh2iAXwYJtjeU3gTQ3HA8KFss",
 	consumer_secret:"YrLMQ0qBrEL0Or3t2ISONOwEuNoszkDkmh5we1huPCsVHXUbFe",
 	access_token_key: "883409045717082112-5gR4ZH0bJtSb4TcT5Nw26CWzivwsbd1",
 	access_token_secret: "GsGI0ykssnWrp14n55UGHJQNCIh16L0h0R7HzcQWaeMgj",
 }
 
-exports.spotifyKeys = {
-	consumer_key: "",
-	consumer_secret: "",
-	access_token_key: "",
-	access_token_secret: "",
+exports.spotifyKey = {
+
 }
 
+exports.omdbKey = {
+	key: "40e9cece"
+}
